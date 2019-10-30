3bcc03b8d77dbbf407db7788b0c9ca8e1c86a732
KirillSN1 <53705733+KirillSN1@users.noreply.github.com>
2019-10-30 22:13:37 +0300

    Анимация

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
- 
+    public float MInput; 
     public float Speed;
     public float JumpTime = 1;
 
@@ -22,13 +22,20 @@ public class PlayerBehaviour : MonoBehaviour
 
     public bool IsAlive = true;
     private Rigidbody2D rb;
-    private bool isGrounded = false;
+    public bool isGrounded = false;
+
+    public Animator anim;
+    private float scale;
+    public Vector2 JVelos;
+    
+    
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 2;
+        scale = transform.localScale.x;
     }
 
 
@@ -39,9 +46,9 @@ public class PlayerBehaviour : MonoBehaviour
             IsAlive = false;
         }
    
-        float MInput = Input.GetAxisRaw("Horizontal");
+        MInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(MInput * Speed, rb.velocity.y);
-            
+        JVelos = rb.velocity;    
         
         if (Input.GetKeyDown(JumpButton) && isGrounded)
         {
@@ -49,11 +56,46 @@ public class PlayerBehaviour : MonoBehaviour
         }
 
         isGrounded = Physics2D.OverlapCircle(Feet.position, feetRadius, Groundlayer);
+        AnimatinCont();
     }   
 
     public void ReceiveDamage(int takenDamage)
     {
         Health -= takenDamage;
+    }
+
+    public void AnimatinCont()
+    {
+        anim.SetFloat("Speed", Mathf.Abs(MInput));
+        anim.SetFloat("JumpVelos",rb.velocity.y);
+        if (MInput < 0)
+        {
+            transform.localScale = new Vector3(-scale, transform.localScale.y, transform.localScale.z);
+        }
+        else if (MInput>0)
+        {
+            transform.localScale = new Vector3(scale, transform.localScale.y, transform.localScale.z);
+        }
+        if (isGrounded == false)
+        {
+            anim.SetBool("isJumped",true);
+        }
+        else anim.SetBool("isJumped", false);
+        if (rb.velocity.y<0)
+        {
+            anim.SetBool("falling", true);
+            anim.SetBool("isJumped", false);
+        }
+        else if (rb.velocity.y>0)
+        {
+            anim.SetBool("falling", false);
+            anim.SetBool("isJumped", true);
+        }
+        if (isGrounded)
+        {
+            anim.SetBool("IsGrounded", true);
+        }
+        else anim.SetBool("IsGrounded", false);
     }
        
 }

b4d775b00476fb4f7c4791a506286a9e9aa858be
kbarketova <38067380+kbarketova@users.noreply.github.com>
2019-10-31 18:49:24 +0300

    EnemyAnimation

--- Assets/Scripts/EnemyBasicAI.cs
+++ Assets/Scripts/EnemyBasicAI.cs
@@ -8,18 +8,23 @@ public class EnemyBasicAI : EnemySettings
 
     public Transform Target;
     public Transform HomePosition;         // позиция, куда возвращается враг, если игрок вышел за пределы ChaseRadius
-    public float StoppingDistance = 1.0f;   // максимальное расстояние, на которое враг может приблизиться к игроку
+    public float StoppingDistance = 1.2f;   // максимальное расстояние, на которое враг может приблизиться к игроку
 
     //public float RetreatDistance = 4.0f;
 
-    private enum MotionDirections { Up, Down, Left, Right };     // для анимации
+    private Animator _anim;
 
-  //  private enum EnemyStates { Idling, Attacking, Walking, Dying };
-  //  private EnemyStates enemyState = EnemyStates.Idling;
+    private enum LookingDirections { Left, Right };     // для анимации
+
+    private enum EnemyStates { Idling, Attacking, Walking, Dying };
+    private EnemyStates enemyState = EnemyStates.Idling;
 
     void Start()
     {
         Target = GameObject.FindGameObjectWithTag("Player").transform;
+        _anim = GetComponent<Animator>();
+
+        _anim.SetBool("isRunningEnemy", false);
     }
 
     
@@ -40,7 +45,6 @@ public class EnemyBasicAI : EnemySettings
 
     public void ChaseThePlayer()
     {
-       
        var distanceToTarget = Vector3.Distance(transform.position, Target.position);
 
         if(distanceToTarget <= ChaseRadius && distanceToTarget != StoppingDistance)  // игрок в зоне преследования
@@ -48,20 +52,56 @@ public class EnemyBasicAI : EnemySettings
             var toTarget = new Vector3(Target.position.x, transform.position.y, 0);
             transform.position = Vector3.MoveTowards(transform.position, toTarget, Speed * Time.deltaTime);
 
-          //  Debug.Log("Enemy chasing!");
+            AnimateRunning(toTarget);
         }
         else if(distanceToTarget > ChaseRadius)          // игрок вышел за пределы радиуса преследования
         {
-           var toHomePosition = new Vector3(HomePosition.position.x, transform.position.y, 0);
-           transform.position = Vector3.MoveTowards(transform.position, toHomePosition, Speed * Time.deltaTime);
+            var toHomePosition = new Vector3(HomePosition.position.x, transform.position.y, 0);
 
-        //    Debug.Log("Enemy going Home!");
+            if (Vector3.Distance(transform.position, toHomePosition) != 0)
+            {
+               transform.position = Vector3.MoveTowards(transform.position, toHomePosition, Speed * Time.deltaTime);
+               AnimateRunning(toHomePosition);          
+            }
+            else
+            {
+                _anim.SetBool("isRunningEnemy", false);      // idle state
+            }
         }
         else     // = distanceToTarget == StoppingDistance
         {
-            // idle state
-         //   Debug.Log("Enemy waiting!");
+            _anim.SetBool("isRunningEnemy", false);      // idle state
         }
      
+    }
+
+    private void AnimateRunning(Vector3 target)
+    {
+        _anim.SetBool("isRunningEnemy", true);
+        
+
+        if (target.x > transform.position.x)
+        {
+            ChooseDirection(LookingDirections.Right);
+        }
+        else if (target.x < transform.position.x)
+        {
+            ChooseDirection(LookingDirections.Left);
+        }
+
+    }
+
+    private void ChooseDirection(LookingDirections motionState)
+    {
+        switch (motionState)
+        {
+            case LookingDirections.Right:
+                _anim.SetFloat("motionH", 1);            
+                break;
+
+            case LookingDirections.Left:
+                _anim.SetFloat("motionH", -1);
+                break;
+        }
     }
 }
--- Assets/Scripts/HurtBox.cs
+++ Assets/Scripts/HurtBox.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class HurtBox : MonoBehaviour
 {
-    public float TimeBetweenAttack = .2f;   // это секунды
+    public float TimeBetweenAttack = .2f;  
     public float TimeTillAttack;
 
     private EnemyBasicAI enemy;
-    // Start is called before the first frame update
+ 
     void Start()
     {
         enemy = transform.parent.GetComponent<EnemyBasicAI>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
@@ -24,7 +24,6 @@ public class HurtBox : MonoBehaviour
     {
         if (other != null && other.gameObject.tag == "Player")
         {
-            //TimeTillAttack -= Time.deltaTime;
             Debug.Log("Time till attack" + TimeTillAttack);
 
             if (TimeTillAttack <= 0)

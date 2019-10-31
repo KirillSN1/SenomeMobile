0653def0259f41aaa1c0fe58421195558dd9fdc0
KirillSN1 <53705733+KirillSN1@users.noreply.github.com>
2019-10-31 23:00:44 +0300

    Сенсорное управление и анимация урона.

---
+++ Assets/Scripts/MobileInput.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MobileInput : MonoBehaviour
+{
+    private PlayerBehaviour main;
+
+    private float MInput;
+    private bool left;
+    private bool right;
+
+
+    private void Start()
+    {
+        main = GetComponent<PlayerBehaviour>();
+    }
+
+    private void Update()
+    {
+        if (left)
+        {
+            MInput = -1;
+        }
+        if (left==false && right==false)
+        {
+            MInput = 0;
+        }
+        if (right)
+        {
+            MInput = 1;
+        }
+        main.MInput = MInput;
+    }
+
+    public void LeftDown()
+    {
+        left = true;
+    }
+    public void LeftUp()
+    {
+        left = false;
+    }
+
+    public void RightDown()
+    {
+       right = true;
+    }
+    public void RightUp()
+    {
+        right = false;
+    }
+    public void Jump()
+    {
+        main.rb.velocity = Vector2.up * main.JumpingVelocity;
+    }
+
+
+
+
+}
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,37 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public float MInput; 
+    public bool KeyboardInput=false; //Управление с клавиатуры
+    public float MInput;
     public float Speed;
     public float JumpTime = 1;
 
     [Range(1, 10)]
     public float JumpingVelocity;
 
-  //  [HideInInspector]
+    //  [HideInInspector]
     public int Health = 5;
 
     public KeyCode JumpButton = KeyCode.Space;
-     
+    public Button Up;
+    public Button Left;
+    public Button Right;
+
     public Transform Feet;
     public float feetRadius;
     public LayerMask Groundlayer;
 
     public bool IsAlive = true;
-    private Rigidbody2D rb;
+    [HideInInspector]
+    public Rigidbody2D rb;
     public bool isGrounded = false;
 
     public Animator anim;
     private float scale;
-    public Vector2 JVelos;
-    
-    
+    //public Vector2 JVelos;
 
-  //  public enum PlayerStates { Idling, Jumping, Attacking, Walking, Dying };
-   // public PlayerStates playerState = PlayerStates.Idling;
+
+
+    //  public enum PlayerStates { Idling, Jumping, Attacking, Walking, Dying };
+    // public PlayerStates playerState = PlayerStates.Idling;
 
     void Start()
     {
@@ -43,65 +49,46 @@ public class PlayerBehaviour : MonoBehaviour
 
     void Update()
     {
-        if(Health <= 0)
+        if (Health <= 0)
         {
             IsAlive = false;
         }
-   
+        Motion();
+        AnimatinCont();
+    }
+
+    public IEnumerator ReceiveDamage(int takenDamage)
+    {
+        Health -= takenDamage;
+        yield return null;
+        anim.SetTrigger("Hit");
+        //   yield return new WaitForSeconds(3f);       // здесь выставить время, которое занимает анимация игрока, получившего урон
+    }
+
+    public void Motion()
+    {
+        if (KeyboardInput)
         MInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(MInput * Speed, rb.velocity.y);
-
-        JVelos = rb.velocity;    
-        
-<<<<<<< HEAD
-
- 
-
-
-=======
->>>>>>> b4d775b00476fb4f7c4791a506286a9e9aa858be
         if (Input.GetKeyDown(JumpButton) && isGrounded)
         {
             rb.velocity = Vector2.up * JumpingVelocity;
         }
 
         isGrounded = Physics2D.OverlapCircle(Feet.position, feetRadius, Groundlayer);
-        AnimatinCont();
-    }   
-
-    public IEnumerator ReceiveDamage(int takenDamage)
-    {
-        Health -= takenDamage;
-        yield return null;
-     //   yield return new WaitForSeconds(3f);       // здесь выставить время, которое занимает анимация игрока, получившего урон
     }
 
     public void AnimatinCont()
     {
         anim.SetFloat("Speed", Mathf.Abs(MInput));
-        anim.SetFloat("JumpVelos",rb.velocity.y);
+        anim.SetFloat("JumpVelos", rb.velocity.y);
         if (MInput < 0)
         {
             transform.localScale = new Vector3(-scale, transform.localScale.y, transform.localScale.z);
         }
-        else if (MInput>0)
+        else if (MInput > 0)
         {
             transform.localScale = new Vector3(scale, transform.localScale.y, transform.localScale.z);
-        }
-        if (isGrounded == false)
-        {
-            anim.SetBool("isJumped",true);
-        }
-        else anim.SetBool("isJumped", false);
-        if (rb.velocity.y<0)
-        {
-            anim.SetBool("falling", true);
-            anim.SetBool("isJumped", false);
-        }
-        else if (rb.velocity.y>0)
-        {
-            anim.SetBool("falling", false);
-            anim.SetBool("isJumped", true);
         }
         if (isGrounded)
         {
@@ -109,5 +96,7 @@ public class PlayerBehaviour : MonoBehaviour
         }
         else anim.SetBool("IsGrounded", false);
     }
+
+    
        
 }

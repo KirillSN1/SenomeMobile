e4604f65b564c8933898d3ee74ee325197a84242
kbarketova <38067380+kbarketova@users.noreply.github.com>
2019-11-04 17:29:33 +0300

    Коммит второй ветки (без изменений)

--- Assets/Scripts/EnemySettings.cs
+++ Assets/Scripts/EnemySettings.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnemyStates { Idling, Attacking, Running, ReceivingDamage, Dying };
+
 public class EnemySettings : MonoBehaviour
 {
-    private enum EnemyStates { Idling, Attacking, Running, ReceivingDamage, Dying };
-    private EnemyStates enemyState = EnemyStates.Idling;
-
+   
     [Header("Enemy Attributes")]
     public string Name = "Kevin";
     public int Attack = 1;    
@@ -15,13 +15,16 @@ public class EnemySettings : MonoBehaviour
   //  [HideInInspector]
     public int Health = 5;       // значение не менять, т.к. здоровье над врагом состоит из 5 сердечек
 
-    // Start is called before the first frame update
+    [HideInInspector]
+    public EnemyStates enemyState = EnemyStates.Idling;
+
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
